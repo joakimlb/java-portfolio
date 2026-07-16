fd4d896ff40a5679dd2206413d127d5d9a900ac5
joakim <joakimbloch0@gmail.com>
2026-07-17 00:32:52 +0200

    guess the number game

--- src/main/java/com/joakim/javaportfolio/JavaPortfolioApplication.java
+++ src/main/java/com/joakim/javaportfolio/JavaPortfolioApplication.java
@@ -8,6 +8,12 @@ public class JavaPortfolioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaPortfolioApplication.class, args);
+
+        int randomNumber = (int) (Math.random() * 100) + 1;
+
+        System.out.println("Welcome to guess the number game!");
+        System.out.println("I'm thinking of a number between 1 and 100.");
+        System.out.println("Can you guess what it is?");
     }
 
 }
