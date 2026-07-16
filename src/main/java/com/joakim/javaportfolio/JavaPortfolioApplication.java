2ebbb7506365de3e0e095fb78e7e4788597c3155
joakim <joakimbloch0@gmail.com>
2026-07-17 00:52:28 +0200

    put game into loop until finished with while loop, added comments

--- src/main/java/com/joakim/javaportfolio/JavaPortfolioApplication.java
+++ src/main/java/com/joakim/javaportfolio/JavaPortfolioApplication.java
@@ -6,16 +6,21 @@ public class JavaPortfolioApplication {
 
     public static void main(String[] args) {
         
+        // Create a Scanner object to read user input
         Scanner scanner = new Scanner(System.in);
 
+        // Generate a random number between 1 and 10
         int randomNumber = (int) (Math.random() * 10) + 1;
 
+        // Game introduction
         System.out.println("Welcome to guess the number game!");
         System.out.println("I'm thinking of a number between 1 and 10.");
         System.out.println("Can you guess what it is?");
         System.out.println("Please input a number between 1 and 10:"); 
 
-        if (scanner.hasNextInt()) {
+        // Loop to allow the user to guess until they input a valid number
+        while (scanner.hasNextInt()) 
+        try {
             int userGuess = scanner.nextInt();
 
             if (userGuess < 1 || userGuess > 10) {
@@ -25,10 +30,12 @@ public class JavaPortfolioApplication {
             } else {
                 System.out.println("Sorry, the correct number was: " + randomNumber);
             } 
-        } else {
-                System.out.println("Invalid input. Please enter a valid number.");
-            }
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter a valid number.");
+            scanner.next(); // Clear the invalid input
+        }
 
+        // Close the scanner and end the game
         System.out.println("Thanks for playing!");
         scanner.close();
     }
