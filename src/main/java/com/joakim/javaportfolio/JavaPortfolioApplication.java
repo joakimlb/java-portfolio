e962eced6f40218138d7ecfb0baa664054fd15f3
joakim <joakimbloch0@gmail.com>
2026-07-17 00:41:45 +0200

    todo fix error

--- src/main/java/com/joakim/javaportfolio/JavaPortfolioApplication.java
+++ src/main/java/com/joakim/javaportfolio/JavaPortfolioApplication.java
@@ -3,17 +3,33 @@ package com.joakim.javaportfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class JavaPortfolioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaPortfolioApplication.class, args);
 
-        int randomNumber = (int) (Math.random() * 100) + 1;
+        Scanner scanner = new Scanner(System.in);
+
+        int randomNumber = (int) (Math.random() * 10) + 1;
 
         System.out.println("Welcome to guess the number game!");
-        System.out.println("I'm thinking of a number between 1 and 100.");
+        System.out.println("I'm thinking of a number between 1 and 10.");
         System.out.println("Can you guess what it is?");
-    }
+        System.out.println("Please input a number between 1 and 10:"); 
 
+        int userGuess = scanner.nextInt();
+
+        if (userGuess !=  || userGuess != 1-10) {
+            System.out.println("Please input a valid number");
+        } else if (userGuess != randomNumber) {
+            System.out.println("Wrong.. better luck next time");
+        } else if (userGuess == randomNumber) {
+            System.out.println("Correct!");
+        }
+
+        System.out.println("Thanks for playing!");
+    }
 }
